21673af0cc1acf82aa9e824bd9f72ff8153c3bf9
BigZoo92 <givernaudenzo@gmail.com>
2024-02-08 09:46:07 +0100

    capacitor init

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'planify.com',
+  appName: 'planify',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
