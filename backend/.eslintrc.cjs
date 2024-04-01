ff99d1cf9ea6709c33245d6cd2ef172278fa4277
BigZoo92 <givernaudenzo@gmail.com>
2024-04-01 23:15:34 +0200

    webscrapping for celcat

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // TypeScript rules
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended' // Active l'intégration de Prettier
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint', // Ajout du plugin TypeScript
+    'prettier' // Active le plugin Prettier
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto'
+      }
+    ],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-unused-vars': 'warn', // Signale les variables non utilisées
+  },
+  
+}
