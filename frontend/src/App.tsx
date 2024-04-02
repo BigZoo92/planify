964b661eda2391e53719151f4a3e7b0e1855b747
QuentinGP <quentgeoffpro@yahoo.com>
2024-04-02 14:45:02 +0200

    fonctions faites tester avec bigo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import './sass/main.scss';
 import TestDialog from './capacitor/dialog/TestDialog';
 import TestCamera from './capacitor/camera/TestCamera';
+import TestGeolocation from './capacitor/geolocalisation/testGeoloc';
+import TestNotification from './capacitor/notification/testNotif';
 function App() {
   return (
     <div>
       <TestDialog />
       <TestCamera />
+      <TestGeolocation />
+      <TestNotification />
     </div>
   );
 }
