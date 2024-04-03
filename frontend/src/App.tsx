8065689e6bee1ece0990b6805dfb27d50ff12714
smoukeuur <enzotang@live.fr>
2024-04-03 20:41:58 +0200

    [dev] calendrier interactif v0.0.3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,46 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './sass/main.scss';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./sass/main.scss";
 
-import Accueil from './components/pages/accueil/Accueil';
-import Calendrier from './components/pages/calendrier/Calendrier';
-import Navbar from './components/custom/navbar/Navbar';
+import Accueil from "./components/pages/accueil/Accueil";
+import Calendrier from "./components/pages/calendrier/Calendrier";
+import Evenement from "./components/pages/evenement/Evenement";
+import Navbar from "./components/custom/navbar/Navbar";
 
 const App: React.FC = () => {
     const [coursFictifs] = useState([
         {
             id: 1,
-            group: 'MMI3-FA-DW',
-            subject: 'Prod. disp. inter. DWA',
-            staff: 'Ben Amor Soufian',
-            classroom: 'I03',
-            date: '2024-04-02',
-            notes: 'Cours de développement web avancé',
-            starttime: '09:00',
-            endtime: '13:00',
+            group: "MMI3-FA-DW",
+            subject: "Prod. disp. inter. DWA",
+            staff: "Ben Amor Soufian",
+            classroom: "I03",
+            date: "2024-04-02",
+            notes: "Cours de développement web avancé",
+            starttime: "09:00",
+            endtime: "13:00",
         },
         {
             id: 2,
-            group: 'MMI3-FA-DW',
-            subject: 'Portfolio CN',
-            staff: 'Ben Amor Soufian',
-            classroom: 'I03',
-            date: '2024-04-02',
-            notes: 'Cours de développement web avancé',
-            starttime: '13:00',
-            endtime: '18:00',
+            group: "MMI3-FA-DW",
+            subject: "Portfolio CN",
+            staff: "Ben Amor Soufian",
+            classroom: "I03",
+            date: "2024-04-02",
+            notes: "Cours de développement web avancé",
+            starttime: "13:00",
+            endtime: "18:00",
         },
         {
             id: 3,
-            group: 'MMI3-FA-DW',
-            subject: 'Portfolio CN',
-            staff: 'Ben Amor Soufian',
-            classroom: 'I03',
-            date: '2024-04-03',
-            notes: 'Cours de développement web avancé',
-            starttime: '13:00',
-            endtime: '18:00',
+            group: "MMI3-FA-DW",
+            subject: "Portfolio CN",
+            staff: "Ben Amor Soufian",
+            classroom: "I03",
+            date: "2024-04-03",
+            notes: "Cours de développement web avancé",
+            starttime: "13:00",
+            endtime: "18:00",
         },
     ]);
 
@@ -55,6 +56,7 @@ const App: React.FC = () => {
                     path="/calendrier"
                     element={<Calendrier coursFictifs={coursFictifs} />}
                 />
+                <Route path="/new" element={<Evenement />} />
             </Routes>
         </Router>
     );
