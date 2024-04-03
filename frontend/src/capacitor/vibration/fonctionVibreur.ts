c24f5491524bdd9bcf59afc779f74ca36405637e
QuentinGP <quentgeoffpro@yahoo.com>
2024-04-03 14:28:38 +0200

    ptttttttttttttn

--- frontend/src/capacitor/vibration/fonctionVibreur.ts
+++ frontend/src/capacitor/vibration/fonctionVibreur.ts
@@ -1,10 +1,9 @@
 import { Haptics } from '@capacitor/haptics';
-// Utiliser ImpactStyle pour changer la puissance
-export async function fonctionVibration() {
+export const fonctionVibration = async () => {
   try {
     await Haptics.vibrate();
     console.log('Vibration triggered');
   } catch (e) {
     console.error('Error triggering vibration', e);
   }
-}
+};
