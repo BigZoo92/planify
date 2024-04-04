97303c70f62b83b7f6b9f432eda7bd72216cc667
BigZoo92 <givernaudenzo@gmail.com>
2024-04-04 09:50:50 +0200

    review

---
+++ frontend/src/pages/LoginHome/LoginHome.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../components/Button";
+
+import loginElement from "../../../assets/images/login/login-element--principal.svg";
+import "./LoginHome.scss";
+
+const LoginHome: React.FC = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="login-wrapper">
+            <img src={loginElement} alt="loginElement" />
+            <h1>Débutez votre aventure</h1>
+            <div className="btn-wrapper">
+                <Button variant="main" onClick={() => navigate("/login")}>
+                    Se connecter
+                </Button>
+                <Button variant="secondary">Créez un compte</Button>
+            </div>
+        </div>
+    );
+};
+
+export default LoginHome;
