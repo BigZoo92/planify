6c4c95d42a58cfc11018dba9d3acae82d7bbaeeb
BigZoo92 <givernaudenzo@gmail.com>
2024-04-05 10:11:07 +0200

    cors backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,12 @@
 import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import compression from 'compression';
+import cors from 'cors';
 import { scrapeTestPage } from './routes/scraper';
 import { parseIcsFile } from './routes/parseIcs';
+import { corsOptions } from './constants';
 
 //For env File
 dotenv.config();
@@ -9,15 +14,21 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+// JSON FORMAT
+app.use(express.json());
+
+// MIDDLEWARE
+app.use(helmet({ crossOriginResourcePolicy: false }));
+app.use(morgan("combined"));
+app.use(compression());
+app.use(cors(corsOptions));
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
-app.get('/scrape', async (req, res) => {
-  const url = 'https://chronos.iut-velizy.uvsq.fr/EDT/g235272.html';
-  const test = await scrapeTestPage(url);
-  res.send(test);
-});
+app.get('/scrape', async (req, res) => await scrapeTestPage(req, res));
 
 app.get('/ics', async (req, res) => {
   const test = await parseIcsFile();
