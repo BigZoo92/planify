70c6aa423c678fa439c8a3da4950f4347a35547f
BigZoo92 <givernaudenzo@gmail.com>
2024-04-24 10:02:34 +0200

    stash trained model

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
-import { scrapeTestPage } from './scraper';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import compression from 'compression';
+import cors from 'cors';
+import { parseIcsFile } from './routes/parseIcs';
+import { corsOptions } from './constants';
+import { getDataFromCelcat } from './routes/getDataFromCelcat';
 
 //For env File
 dotenv.config();
@@ -12,9 +18,11 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
-app.get('/scrape', async (req, res) => {
-  const url = 'https://chronos.iut-velizy.uvsq.fr/EDT/g235272.html';
-  const test = await scrapeTestPage(url);
+// app.post('/scrape', async (req, res) => await scrapeTestPage(req, res));
+app.post('/getDataFromCelcat', async (req, res) => await getDataFromCelcat(req, res));
+
+app.get('/ics', async (req, res) => {
+  const test = await parseIcsFile();
   res.send(test);
 });
 
---
+++ frontend/src/pages/Accueil/Accueil.tsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import CalendarCard from "../../components/CardCalendrier/CardCalendrier";
+import "./Accueil.scss";
+import SearchBar from "../../components/SearchBar/Searchbar";
+import { CalendarEvent, getTimetableFromUrl } from "../../utils/queries";
+
+const Accueil = () => {
+    const [timetables, setTimetables] = useState<CalendarEvent[]>([]);
+
+    useEffect(() => {
+        if (timetables.length) return;
+        (async () => {
+            const newTimetables = await getTimetableFromUrl(
+                "https://chronos.iut-velizy.uvsq.fr/EDT/g235272.html"
+            );
+            console.log(newTimetables);
+            setTimetables(newTimetables);
+        })();
+    }, [timetables, setTimetables]);
+
+    const dateActuelle = new Date();
+    const optionsJour: { weekday: "long" | "short" | "narrow" } = {
+        weekday: "long",
+    };
+    const nomDuJour = dateActuelle.toLocaleDateString("fr-FR", optionsJour);
+    const optionsDate: {
+        day: "2-digit" | "numeric";
+        month: "long" | "short" | "narrow";
+    } = {
+        day: "2-digit",
+        month: "long",
+    };
+    const dateDuJour = dateActuelle.toLocaleDateString("fr-FR", optionsDate);
+
+    return (
+        <div className="page-wrapper accueil">
+            <div className="accueil-content-wrapper">
+                <h1 className="accueil-date">{dateDuJour}</h1>
+                <h2 className="accueil-date-nom">
+                    {nomDuJour.charAt(0).toUpperCase() + nomDuJour.slice(1)}
+                </h2>
+                <div className="meteo-wrapper">
+                    <div className="meteo-infos">
+                        <h2>Météo</h2>
+                        <span>15 °C</span>
+                        <div className="meteo-location">
+                            <h3>Localisation</h3>
+                            <p>Vélizy Villacoublay</p>
+                        </div>
+                    </div>
+                    <div className="meteo-soleil">
+                        <div className="meteo-content">
+                            <h3>Lever du soleil</h3>
+                            <p>07:19</p>
+                        </div>
+                        <div className="meteo-content">
+                            <h3>Coucher du soleil</h3>
+                            <p>20:26</p>
+                        </div>
+                    </div>
+                </div>
+                <SearchBar onSearch={() => console.log("wesh")} />
+                <h2>Prochains évènements</h2>
+                <div className="calendar-wrapper">
+                    {timetables.length > 0 ? (
+                        timetables.map((timetable, index) => (
+                            <CalendarCard
+                                key={index}
+                                group={timetable.data.group}
+                                subject={timetable.summary}
+                                staff={timetable.data.staff}
+                                classroom={timetable.location}
+                                date={timetable.data.date}
+                                notes={timetable.data.notes}
+                                starttime={timetable.start}
+                                endtime={timetable.end}
+                            />
+                        ))
+                    ) : (
+                        <p className="cours-absent">Aucun cours à venir.</p>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Accueil;
