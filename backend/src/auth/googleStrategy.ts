ef74a83ab8c475832f5816020b9ec57f4f6bcbd8
BigZoo92 <givernaudenzo@gmail.com>
2024-04-24 10:52:01 +0200

    review

--- backend/src/auth/googleStrategy.ts
+++ backend/src/auth/googleStrategy.ts
@@ -1,5 +1,6 @@
 import { OAuth2Client } from 'google-auth-library';
 import dotenv from 'dotenv';
+import { Request, Response } from 'express';
 import { google } from 'googleapis'; 
 
 dotenv.config();
@@ -10,7 +11,7 @@ const client = new OAuth2Client(
   'http://localhost:8000/auth/google/callback' 
 );
 
-export const getGoogleAuthURL = (): string => {
+export const getGoogleAuthURL = (req: Request, res: Response) => {
   const url = client.generateAuthUrl({
     access_type: 'offline',
     prompt: 'consent',
@@ -19,16 +20,27 @@ export const getGoogleAuthURL = (): string => {
       'https://www.googleapis.com/auth/userinfo.profile',
     ],
   });
-  return url;
+  res.redirect(url)
 };
 
-export const getGoogleUser = async (code: string) => {
-  const { tokens } = await client.getToken(code);
+export const getGoogleUser = async (req: Request, res: Response) => {
+  const { code } = req.query;
+  if (code) {
+    try {
+  const { tokens } = await client.getToken(code as string);
   client.setCredentials(tokens);
   const oauth2 = google.oauth2({
     version: 'v2',
     auth: client,
   });
   const { data } = await oauth2.userinfo.get();
-  return data;
+  return data
+}
+  catch (error) {
+    console.error('Erreur lors de l\'échange de code Google :', error);
+    res.status(500).send('Erreur d\'authentification avec Google.');
+  }
+} else {
+  res.status(400).send('Code non fourni.');
+}
 };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,27 +13,8 @@ import { handleAppleLogin, getAppleLoginUrl } from './auth/appleStrategy';
 dotenv.config();
 
 
-const app: Application = express();
+const app = express();
 const port = process.env.PORT || 8000;
-const appleConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, './appleAuthConfig.json'), 'utf8'));
-const appleAuth = new AppleAuth(appleConfig, appleConfig.key_id);
-
-
-app.get('/auth/apple', (req: Request, res: Response) => {
-  const url = getAppleLoginUrl();
-  res.redirect(url);
-});
-app.post('/auth/apple/callback', async (req: Request, res: Response) => {
-  try {
-    const response = await appleAuth.accessToken(req.body.code);
-    const appleUser = await appleAuth.verifyIdToken(response.id_token);
-    req.session.user = appleUser; 
-    res.redirect('/some-internal-page');
-  } catch (error) {
-    console.error('Apple login error:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
 
 app.use(session({
   secret: process.env.SESSION_SECRET || 'secret', 
@@ -45,26 +26,9 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
-app.get('/auth/google', (req: Request, res: Response) => {
-  const url = getGoogleAuthURL();
-  res.redirect(url);
-});
+app.get('/auth/google', (req: Request, res: Response) => getGoogleAuthURL(req, res));
 
-app.get('/auth/google/callback', async (req: Request, res: Response) => {
-  const { code } = req.query;
-  if (code) {
-    try {
-      const user = await getGoogleUser(code.toString());
-      req.session.user = user; 
-      res.redirect('/some-internal-page'); 
-    } catch (error) {
-      console.error('Erreur lors de l\'échange de code Google :', error);
-      res.status(500).send('Erreur d\'authentification avec Google.');
-    }
-  } else {
-    res.status(400).send('Code non fourni.');
-  }
-});
+app.get('/auth/google/callback', async (req: Request, res: Response) => getGoogleUser(req, res));
 
 app.get('/scrape', async (req, res) => {
   const url = 'https://chronos.iut-velizy.uvsq.fr/EDT/g235272.html';
