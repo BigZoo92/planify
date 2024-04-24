fa4346d93cf67a8790721e7dd09d7eacec4442ed
Noe Ledoux <LEDOUN01@heiway.net>
2024-04-24 09:55:18 +0200

    final commit for this branch

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,15 +3,37 @@ import dotenv from 'dotenv';
 import { scrapeTestPage } from './scraper';
 import session from 'express-session';
 import { getGoogleAuthURL, getGoogleUser } from './auth/googleStrategy'; 
-import SessionData from './types';
+import SessionData from './@types/types';
+import AppleAuth from 'apple-auth';
+import fs from 'fs';
+import path from 'path';
+import { handleAppleLogin, getAppleLoginUrl } from './auth/appleStrategy';
+
 
 dotenv.config();
 
-console.log(process.env.GOOGLE_CLIENT_ID);
-console.log(process.env.GOOGLE_CLIENT_SECRET);
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+const appleConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, './appleAuthConfig.json'), 'utf8'));
+const appleAuth = new AppleAuth(appleConfig, appleConfig.key_id);
+
+
+app.get('/auth/apple', (req: Request, res: Response) => {
+  const url = getAppleLoginUrl();
+  res.redirect(url);
+});
+app.post('/auth/apple/callback', async (req: Request, res: Response) => {
+  try {
+    const response = await appleAuth.accessToken(req.body.code);
+    const appleUser = await appleAuth.verifyIdToken(response.id_token);
+    req.session.user = appleUser; 
+    res.redirect('/some-internal-page');
+  } catch (error) {
+    console.error('Apple login error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.use(session({
   secret: process.env.SESSION_SECRET || 'secret', 
