c647a7ece767c333d80e0216d5ba8a32fbc57c69
Noe Ledoux <LEDOUN01@heiway.net>
2024-04-24 11:28:11 +0200

    first commit WebSocket

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-
 import session from 'express-session';
 import helmet from 'helmet';
 import morgan from 'morgan';
@@ -8,6 +7,13 @@ import compression from 'compression';
 import cors from 'cors';
 import router from './routes';
 import { corsOptions } from './constant';
+import { Server as SocketIOServer } from 'socket.io';
+import { createServer } from 'http';
+import { setupWebsocketServer } from './socket'; 
+import session from 'express-session';
+import { Server as SocketIOServer } from 'socket.io';
+import { createServer } from 'http';
+import { setupWebsocketServer } from './socket';  
 
 dotenv.config();
 
@@ -20,7 +26,21 @@ app.use(compression());
 app.use(morgan('dev'));
 
 app.use(express.json());
+const server = createServer(app);
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]
+  }
+});
 
+setupWebsocketServer(io);
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'secret', 
+  resave: true,
+  saveUninitialized: true,
+}));
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
