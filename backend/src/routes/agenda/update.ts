2979cf15838e5f05a1fe72ac49121fe49ab93744
BigZoo92 <givernaudenzo@gmail.com>
2024-04-26 11:48:35 +0200

    (backend)-Feat: add endpoints for events and agenda

---
+++ backend/src/routes/agenda/update.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { AgendaSchema } from "../../schema"; 
+import { prisma } from "../../schema/prismaClient";
+
+export const update = async (req: Request<{ id: number }, {}>, res: Response) => {
+  try { 
+
+    const updateData = AgendaSchema.parse(req.body); 
+
+    const existingAgenda = await prisma.agenda.findUnique({
+      where: { id: updateData.id },
+    });
+
+    if (!existingAgenda) {
+      return res.status(404).json({ message: "Agenda with provided ID not found." });
+    }
+
+    const updatedAgenda = await prisma.agenda.update({
+      where: { id: updateData.id },
+      data: updateData,
+    });
+
+    res.status(200).json({ agenda: updatedAgenda });
+  } catch (error: any) {
+    console.error("Error updating agenda:", error);
+    res.status(500).json({ message: "Error updating agenda" });
+  }
+};
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { authRouter } from "./auth";
 import { timetableRouter } from "./timetable";
+import { eventsRouter } from "./events";
+import { agendaRouter } from "./agenda";
 
 const router = express.Router();
 
 router.use('/auth', authRouter)
 router.use('/timetable', timetableRouter)
+router.use('/events', eventsRouter)
+router.use('/agenda', agendaRouter)
 
 export default router;
