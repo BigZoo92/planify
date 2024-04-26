c45b42022a12bd4dd51d0ce306133cc24b961447
smoukeuur <enzotang@live.fr>
2024-04-26 14:40:04 +0200

    [merge request]

--- frontend/src/utils/capacitor/dialog/dialog.ts
+++ frontend/src/utils/capacitor/dialog/dialog.ts
@@ -1,12 +1,12 @@
-import { Dialog } from '@capacitor/dialog';
+import { Dialog } from "@capacitor/dialog";
 export const dialog = async (title: string, message: string) => {
-  try {
-    await Dialog.alert({
-      title: title,
-      message: message,
-    });
-    return true;
-  } catch (error) {
-    return error;
-  }
+    try {
+        await Dialog.alert({
+            title: title,
+            message: message,
+        });
+        return true;
+    } catch (error) {
+        return error;
+    }
 };
