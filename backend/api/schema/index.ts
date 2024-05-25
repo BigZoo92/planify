f6dc0883c2ea4ee4779435d48cb04fbb474bf5fd
BigZoo92 <givernaudenzo@gmail.com>
2024-05-25 17:39:56 +0200

    test vercel v17

--- backend/api/schema/index.ts
+++ backend/api/schema/index.ts
@@ -1,57 +1,5 @@
 import { z } from 'zod';
-import { Prisma } from '@prisma/client';
 
-/////////////////////////////////////////
-// HELPER FUNCTIONS
-/////////////////////////////////////////
-
-// JSON
-//------------------------------------------------------
-
-export type NullableJsonInput = Prisma.JsonValue | null | 'JsonNull' | 'DbNull' | Prisma.NullTypes.DbNull | Prisma.NullTypes.JsonNull;
-
-export const transformJsonNull = (v?: NullableJsonInput) => {
-  if (!v || v === 'DbNull') return Prisma.DbNull;
-  if (v === 'JsonNull') return Prisma.JsonNull;
-  return v;
-};
-
-export const JsonValueSchema: z.ZodType<Prisma.JsonValue> = z.lazy(() =>
-  z.union([
-    z.string(),
-    z.number(),
-    z.boolean(),
-    z.literal(null),
-    z.record(z.lazy(() => JsonValueSchema.optional())),
-    z.array(z.lazy(() => JsonValueSchema)),
-  ])
-);
-
-export type JsonValueType = z.infer<typeof JsonValueSchema>;
-
-export const NullableJsonValue = z
-  .union([JsonValueSchema, z.literal('DbNull'), z.literal('JsonNull')])
-  .nullable()
-  .transform((v) => transformJsonNull(v));
-
-export type NullableJsonValueType = z.infer<typeof NullableJsonValue>;
-
-export const InputJsonValueSchema: z.ZodType<Prisma.InputJsonValue> = z.lazy(() =>
-  z.union([
-    z.string(),
-    z.number(),
-    z.boolean(),
-    z.object({ toJSON: z.function(z.tuple([]), z.any()) }),
-    z.record(z.lazy(() => z.union([InputJsonValueSchema, z.literal(null)]))),
-    z.array(z.lazy(() => z.union([InputJsonValueSchema, z.literal(null)]))),
-  ])
-);
-
-export type InputJsonValueType = z.infer<typeof InputJsonValueSchema>;
-
-
-/////////////////////////////////////////
-// ENUMS
 /////////////////////////////////////////
 
 export const TransactionIsolationLevelSchema = z.enum(['ReadUncommitted','ReadCommitted','RepeatableRead','Serializable']);
@@ -74,13 +22,11 @@ export const MessageScalarFieldEnumSchema = z.enum(['id','content','senderId','r
 
 export const SortOrderSchema = z.enum(['asc','desc']);
 
-export const JsonNullValueInputSchema = z.enum(['JsonNull',]).transform((value) => (value === 'JsonNull' ? Prisma.JsonNull : value));
 
 export const QueryModeSchema = z.enum(['default','insensitive']);
 
 export const NullsOrderSchema = z.enum(['first','last']);
 
-export const JsonNullValueFilterSchema = z.enum(['DbNull','JsonNull','AnyNull',]).transform((value) => value === 'JsonNull' ? Prisma.JsonNull : value === 'DbNull' ? Prisma.JsonNull : value === 'AnyNull' ? Prisma.AnyNull : value);
 
 export const AgendaTypeSchema = z.enum(['UNIVERSITAIRE','PERSONNEL']);
 
@@ -154,7 +100,7 @@ export const EventSchema = z.object({
   location: z.string(),
   start: z.string(),
   end: z.string(),
-  data: JsonValueSchema.nullable(),
+  data: z.any(),
   universityDataId: z.number().nullable(),
   createdAt: z.date(),
   updatedAt: z.date(),
