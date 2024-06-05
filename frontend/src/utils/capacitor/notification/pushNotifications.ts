bea02007d68cfde4c5d7cdbe4d3f47e9a84abab1
Enzo Givernaud <enzo.givernaud@iktos.com>
2024-06-05 17:36:35 +0200

    fix-push token for firebase

--- frontend/src/utils/capacitor/notification/pushNotifications.ts
+++ frontend/src/utils/capacitor/notification/pushNotifications.ts
@@ -6,23 +6,23 @@ import axios from "axios";
 import { requestForToken } from "../../../firebaseConfig";
 
 export const registerPushNotifications = async (userId: number) => {
-    const isPushNotificationsAvailable = Capacitor.isPluginAvailable("PushNotifications");
-    
+    const isPushNotificationsAvailable =
+        Capacitor.isPluginAvailable("PushNotifications");
     if (!isPushNotificationsAvailable) {
         console.error("PushNotifications plugin is not available");
         return;
     }
-    
+
     console.log("isAvailable For notification");
-    
+
     const result = await PushNotifications.requestPermissions();
     if (result.receive !== "granted") {
         console.error("Push notification permission denied");
         return;
     }
-    
+    console.log("Push notification permission accepted");
     PushNotifications.register();
-
+    console.log("Push notification is register");
     PushNotifications.addListener("registration", async (token) => {
         console.log("Push registration success, token: " + token.value);
         await axios.post(
@@ -33,19 +33,25 @@ export const registerPushNotifications = async (userId: number) => {
             }
         );
     });
-
+    console.log("Push notification's query maked");
     PushNotifications.addListener("registrationError", (error) => {
         console.error("Push registration error: " + error);
     });
 
-    PushNotifications.addListener("pushNotificationReceived", (notification) => {
-        console.log("Push received: ", notification);
-        alert("Notification: " + notification.body);
-    });
+    PushNotifications.addListener(
+        "pushNotificationReceived",
+        (notification) => {
+            console.log("Push received: ", notification);
+            alert("Notification: " + notification.body);
+        }
+    );
 
-    PushNotifications.addListener("pushNotificationActionPerformed", (notification) => {
-        console.log("Push action performed: " + notification);
-    });
+    PushNotifications.addListener(
+        "pushNotificationActionPerformed",
+        (notification) => {
+            console.log("Push action performed: " + notification);
+        }
+    );
 
     // Request the Firebase token for web notifications
     await requestForToken();
